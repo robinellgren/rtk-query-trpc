78bd9e7e2c5ba99c050646f617b769171b633ca0
Robin Ellgren <robin.ellgren@mindler.se>
2023-03-05 10:51:08 +0100

    Finish up pokemon

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,55 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, { useState } from 'react';
+import { useGetPokemonByNameQuery } from './app/services/pokemon';
 import './App.css';
+import styles from './Pokemon.module.css';
+
+const getNameFromInput = (input:string|undefined) => {
+  if(input === undefined) {
+    console.warn("INPUT IS UNDEFINED", input);
+    return "";
+  }
+  return input;
+}
 
 function App() {
+  const [pokemonName, setPokemonName] = useState('bulbasaur');
+  const { data, error, isLoading } = useGetPokemonByNameQuery(pokemonName);
+  let emptyInput = pokemonName === "";
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Welcome to the Pokemon page that uses RTK Query
         </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+        <div className={styles.row}>
+          <input
+            className={styles.textbox}
+            aria-label="Which pokemon do you want to know about?"
+            value={pokemonName}
+            onChange={(e) => setPokemonName(getNameFromInput(e.target.value))}
+          />
+        </div>
+        <div>
+          {isLoading || emptyInput ? (
+            <div></div>
+          ) : (
+            <div>
+              {data?.sprites ? (
+                <img src={data?.sprites.front_shiny} className="pokemon-img" alt="poke" width="400" height="500" />
+              ) : (
+                <b>Error: No such pokemon: {pokemonName}</b>
+              )}
+            </div>
+          )}
+        </div>
       </header>
     </div>
   );
