92b8a749eed1d4cdeb859dc6e403cc97872b32eb
Robin Ellgren <robin.ellgren@mindler.se>
2023-03-05 12:07:13 +0100

    Add apiUrlReducer

--- packages/frontend/src/app/store.ts
+++ packages/frontend/src/app/store.ts
@@ -3,13 +3,22 @@ import { ThunkAction, Action } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import { pokemonApi } from "./services/pokemon";
 import { nameApi } from "./services/names";
+import apiUrlReducer from "./services/apiUrlReducer";
+
+const preloadedState = {
+  apiUrls: {
+    TRPC_API_URL: "http://localhost:4000/trpc",
+  },
+};
 
 export const store = rtkConfigureStore({
   reducer: {
+    apiUrls: apiUrlReducer,
     [pokemonApi.reducerPath]: pokemonApi.reducer,
     [nameApi.reducerPath]: nameApi.reducer,
     counter: counterReducer,
   },
+  preloadedState,
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
   middleware: (getDefaultMiddleware) =>
